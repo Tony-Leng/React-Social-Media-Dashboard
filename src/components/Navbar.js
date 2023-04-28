c5cd8653d7f76e7510c0c16a39fd318bd0a8839f
Tony Leng <lengtk@gmail.com>
2023-04-28 17:37:54 +0900

    started building Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="title">
+        <h2>Social Media Dashboard</h2>
+        <h6>Total Followers: 23,004</h6>
+      </div>
+      <div className="darkmode">
+        <h4>Dark Mode</h4>
+        <button>Toggler</button>
+      </div>
+    </div>
+  );
+}
