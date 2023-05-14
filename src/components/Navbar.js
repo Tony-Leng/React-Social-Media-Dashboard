df26bae45c22cd84f85dcbc0eb827a3a5114d61d
Tony Leng <lengtk@gmail.com>
2023-05-14 15:47:54 +0900

    fixed auto refresh bug by moving env file to root folder of project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 
 export default function Navbar() {
   const toggleButton = () => {
-    console.log("Clicked")
+    document.querySelector('body').classList.add('.darkmode')
   }
   return (
     <div className="navbar">
@@ -9,7 +9,7 @@ export default function Navbar() {
         <h2>Social Media Dashboard</h2>
         <h6>Total Followers: 23,004</h6>
       </div>
-      <div className="darkmode">
+      <div className="toggler">
         <h4>Dark Mode</h4>
         <button onClick={toggleButton}>Toggler</button>
       </div>
